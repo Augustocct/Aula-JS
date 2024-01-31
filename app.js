da41ebf5bf3eac179b1ad5702d005b203d3f96d8
Augusto Conte Cargnin Teixeira <118365464+Augustocct@users.noreply.github.com>
2024-01-31 11:16:22 -0300

    Aula 3

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 alert("Boas vindas ao jogo do numero secreto");
 let numeroSecreto = 9;
-console.log(numeroSecreto)
-let chute = prompt("Insira um numero entre 1 e 10");
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-//se o chute for igual a o numero secreto
+//enquanto o chute nao for igual ao n. s.
+while (chute != numeroSecreto) {
+    chute = prompt("Insira um numero entre 1 e 10");
+    //se o chute for igual a o numero secreto
 if (chute == numeroSecreto) {
-    alert(`Isso ae! Voce descobriu o numero secreto ${numeroSecreto}`);
+    alert(`Isso ae! Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
 } else {
-    alert("voce errou:")
+    if (chute > numeroSecreto) {
+        alert(`o numero secreto é menor do que ${chute}`);
+    } else {
+        alert(`o numero secreto é maior do que ${chute}`);
+    }
+    //tentativas= tentativas + 1;
+    tentativas++;
+}
 }
