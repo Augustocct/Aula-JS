d51c5e4053550d594c7f146372173b241f414c9b
Augusto Conte Cargnin Teixeira <118365464+Augustocct@users.noreply.github.com>
2024-01-31 16:34:39 -0300

    Aula 4

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 alert("Boas vindas ao jogo do numero secreto");
-let numeroSecreto = 9;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 //enquanto o chute nao for igual ao n. s.
 while (chute != numeroSecreto) {
-    chute = prompt("Insira um numero entre 1 e 10");
+    chute = prompt("Insira um numero entre 1 e 100");
     //se o chute for igual a o numero secreto
 if (chute == numeroSecreto) {
-    alert(`Isso ae! Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    break;
 } else {
     if (chute > numeroSecreto) {
         alert(`o numero secreto é menor do que ${chute}`);
@@ -20,3 +20,8 @@ if (chute == numeroSecreto) {
     tentativas++;
 }
 }
+
+//operador ternario, mercado usa assim ao inves de muitos if e else para uma mudanca pequena na condicao
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
+alert(`Isso ae! Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
